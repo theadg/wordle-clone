e696baaa98b92d0f3410d1328c47b39ec6d16f7d
theadg <drewndrei@gmail.com>
2022-08-11 16:59:46 +0800

    Add font,height and width configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    fontFamily:{
+      'poppins' : ['Poppins', 'sans-serif'],
+    },
     extend: {
+      height: {
+        'ht': '440px',
+        'hk': '200px',
+      },
+      width: {
+        'wt': '330px',
+        'wk': '484px',
+        '18': '70px', 
+      },
       colors: {
         yellow: 'rgb(181, 159, 59)',
         green: 'rgb(83, 141, 78)',
