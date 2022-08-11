18379f5bd4f6bc2dc5e9d68505ad8b6a74d5f05d
theadg <drewndrei@gmail.com>
2022-08-11 14:34:44 +0800

    Set up tailwind and add necessary files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        yellow: 'rgb(181, 159, 59)',
+        green: 'rgb(83, 141, 78)',
+        gray: 'rgb(129, 131, 132)',
+        blackWrong: 'rgb(58,58,60)',
+      },
+
+    },
+  },
+  plugins: [],
+}
